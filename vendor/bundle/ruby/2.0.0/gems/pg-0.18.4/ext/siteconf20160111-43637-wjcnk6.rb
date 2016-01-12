6c9e816115ad907ec8b3863d2bb8ad04ef19c85d
Jessica King <jking@phoenixdirect.com>
2016-01-11 20:38:32 -0500

    first commit

---
+++ vendor/bundle/ruby/2.0.0/gems/pg-0.18.4/ext/siteconf20160111-43637-wjcnk6.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160111-43637-p9jpor"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
